aab4f8cf9e786855f7cac56c23a6ecd9032bd017
kekonline <kekonline@gmail.com>
2024-04-11 22:19:42 +0200

    web scraping

---
+++ 3/index.js
@@ -0,0 +1,10 @@
+const request = require('request-promise').defaults({
+    proxy: 'http://191.37.227.128:8080'
+    // Format:  http://username:password@ip:port
+});
+
+(async () => {
+    let response = await request('https://httpbin.org/ip');
+ 
+    console.log(response);
+})();
