f54a4787ca8af80dc9859870bb2ca094afa01381
kekonline <kekonline@gmail.com>
2024-04-14 09:24:23 +0200

    web scraping

---
+++ 6/62code/code/index.js
@@ -0,0 +1,17 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({
+      headless: false
+  });
+  const page = await browser.newPage();
+  await page.goto('https://google.com/');
+  await page.type('#lst-ib', 'Udemy Tutorials', {delay: 100});
+  await page.keyboard.press('Enter');
+  await page.waitForNavigation();
+  
+  await page.screenshot({path: 'example.png'});
+
+  await browser.close();
+
+})();
