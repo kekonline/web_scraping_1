cf88ecd7cc68b89a87d05d5ab2456c3854dbe4dd
kekonline <kekonline@gmail.com>
2024-04-21 10:37:10 +0200

    web scraping

---
+++ 8/code (1)/code/amazon.js
@@ -0,0 +1,60 @@
+const puppeteer = require('puppeteer');
+
+let browser = null;
+let page = null;
+
+/* Constants */
+const BASE_URL = 'https://amazon.com/';
+
+const amazon = {
+
+    initialize: async () => {
+        console.log('Starting the scraper..');
+
+        browser = await puppeteer.launch({
+            headless: false
+        })
+        
+        page = await browser.newPage();
+
+        await page.goto(BASE_URL, { waitUntil: 'networkidle2' });
+
+        console.log('Initialization completed..');
+    },
+
+    getProductDetails: async (link) => {
+
+        console.log(`Going to the Product Page.. ( ${link} )`);
+
+        await page.goto(link, { waitUntil: 'networkidle2' });
+
+        let details = await page.evaluate(() => {
+            
+            let title = document.querySelector('#productTitle').innerText;
+            let manufacturer = document.querySelector('#bylineInfo').innerText;
+            let currentPrice = document.querySelector('#priceblock_ourprice').innerText;
+            let rating = document.querySelector('#acrPopover').getAttribute('title');
+            let totalRatings = document.querySelector('#acrCustomerReviewText').innerText;
+
+            return {
+                title,
+                manufacturer,
+                currentPrice,
+                rating,
+                totalRatings
+            }
+
+        });
+
+        return details;
+    },
+
+    end: async () => {
+        console.log('Stopping the scraper..');
+        
+        await browser.close();
+    }
+
+}
+
+module.exports = amazon;
---
+++ 8/code (1)/code/index.js
@@ -0,0 +1,12 @@
+const amazon = require('./amazon');
+
+(async () => {
+  
+  await amazon.initialize();
+
+  let details = await amazon.getProductDetails('https://www.amazon.com/Thule-Crossover-32L-Backpack-Black/dp/B004XANKVO');
+
+  debugger;
+  
+  
+})();
