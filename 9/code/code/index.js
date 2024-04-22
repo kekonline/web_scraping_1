f9706eda039ec58ffa2f462b6320ab4d22301663
kekonline <kekonline@gmail.com>
2024-04-22 20:43:13 +0200

    web scraping

---
+++ 9/code/code/index.js
@@ -0,0 +1,20 @@
+const Nightmare = require('nightmare')
+const nightmare = Nightmare({ show: true }); // { headless: false }
+
+(async () => {
+
+  try {
+    await nightmare.goto('https://duckduckgo.com');
+    await nightmare.type('#search_form_input_homepage', 'github nightmare');
+    await nightmare.click('#search_button_homepage');
+    await nightmare.wait('#r1-0 a.result__a');
+    let link = await nightmare.evaluate(() => document.querySelector('#r1-0 a.result__a').href);
+    await nightmare.end();
+
+    console.log(link);
+  } catch(error) {
+    console.log(`Something happened: ${error}`);
+  }
+
+})();
+
